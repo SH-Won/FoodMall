f08266815f9237ce2299c10507728fa284c8b53e
SH-Won <1317good@naver.com>
2021-04-16 23:33:59 +0900

    test deploy

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,6 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
+    mongoURI:process.env.mongoURI,
+    cloud_name:process.env.cloud_name,
+        api_key:process.env.api_key,
+        api_secret:process.env.api_secret
 }
